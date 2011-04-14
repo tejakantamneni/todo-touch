9ac0beca25b44ef166f64818b09044d57bcbab4e
Teja Kantamneni <Teja.Kantamneni@gmail.com>
2011-04-13 22:52:19 -0400

    added add item form

--- todo-app/war/js/application.js
+++ todo-app/war/js/application.js
@@ -3,68 +3,108 @@ Ext.regModel('TodoItem', {
     fields: ['todoText', 'dueDate']
 });
 
-// A store for the todo items to display. TODO:Make data dynamic from db
-// backend.
-var store = new Ext.data.JsonStore({
-    model  : 'TodoItem',
-    sorters: 'todoText',
 
-    getGroupString : function(record) {
-        return record.get('todoText')[0];
-    },
+Ext.setup({
+	icon: 'icon.png',
+    glossOnIcon: false,
+    tabletStartupScreen: 'tablet_startup.png',
+    phoneStartupScreen: 'phone_startup.png',
+    onReady: function() {
+    	var buttonsSpec = [ { ui: 'confirm', text: 'Add' } ];
+    	var overlay = new Ext.Panel({
+            floating: true,
+            modal: true,
+            centered: false,
+            width: Ext.is.Phone ? 260 : 460,
+            styleHtmlContent: true,
+            scroll: 'vertical',
+            contentEl: 'addItemForm',
+            cls: 'htmlcontent'
+        });
+    	var showOverlay = function(btn, event) {
+    		var v = Ext.get('addItemForm').show();
+    	    overlay.setCentered(false);
+    	    overlay.showBy(btn);
+    	};
+    	var showCenteredOverlay = function(btn, event) {
+    	    overlay.setCentered(true);
+    	    overlay.show();
+    	};
+    	var tapHandler = function (btn, evt) {
+    	    //alert("Button '" + btn.text + "' tapped.");
+    		showOverlay(btn, evt);
+    	}
+    	var j_store = new Ext.data.Store({
+    	    model  : 'TodoItem',
+    	    sorters: 'todoText',
+    	    getGroupString : function(record) {
+    	        return record.get('todoText')[0];
+    	    },
+    	    proxy: {
+    	        type: 'ajax',
+    	        url: '/GetAllItems',
+    	        reader: {
+    	            type: 'json',
+    	        }
+    	    },
+    	    autoLoad:true
+    	});
 
-    data: [
-        {todoText: 'Finish Sencha',   dueDate: 'Study and finish sample app using Sencha'},
-        {todoText: 'Learn Google AppEngine',   dueDate: 'Study AppEngine and JDO store'},
-        {todoText: 'Learn HTML 5',     dueDate: 'Learn HTML 5 n study SqlLite and Location Services'}
-    ]
+    	var dockedItems = [{
+    	    xtype: 'toolbar',
+    	    title: 'Todo Manager',
+    	    ui: 'dark',
+    	    dock: 'top',
+    	    items: buttonsSpec,
+    	    defaults: { handler: tapHandler }
+    	}];
+
+    	// The data list
+    	var list = new Ext.List({
+    	    fullscreen: true,
+    	    itemTpl : '{todoText} {dueDate}',
+    	    grouped : true,
+    	    indexBar: true,
+    	    store: j_store
+    	});
+
+    	new Ext.Application({
+    	        launch: function() {
+    	        	list.show();
+    	        	// Main Panel
+    	        	var myPanel = new Ext.Panel({
+    	        		fullscreen: true,
+    	        		style: 'background-color:#E3E4FA;',
+    	        		items: [list],
+    	        	    dockedItems: dockedItems,
+    	        	});
+    	        }
+    	    });
+    	}
 });
-
-var j_store = new Ext.data.Store({
-    model  : 'TodoItem',
-    sorters: 'todoText',
-
-    getGroupString : function(record) {
-        return record.get('todoText')[0];
-    },
-
-    proxy: {
-        type: 'ajax',
-        url: 'allitems.json',
-        reader: {
-            type: 'json',
-            root : 'items'
-        }
-    },
-    autoLoad:true
+Ext.onReady(function(){
+	var form = new Ext.form.FormPanel({
+		renderTo: 'addItemForm',
+        width: Ext.is.Phone ? 260 : 400,
+		labelWidth:60,
+		id:'newItemForm',
+	    items: [
+	        {
+	            xtype: 'textareafield',
+	            name : 'details',
+	            label: 'Details'
+	        },
+	        {
+	            xtype: 'datepickerfield',
+	            name : 'dueDate',
+	            label: 'Due On',
+	            picker: {yearFrom: 2011}
+	        },{
+	        	xtype: 'button', flex: 1, 
+	        	text: 'Save'
+	        }
+	    ]
+	});
+	
+	Ext.get('addItemForm').hide()
 });
-// The data list
-var list = new Ext.List({
-    fullscreen: true,
-    itemTpl : '{todoText} {dueDate}',
-    grouped : true,
-    indexBar: true,
-    store: j_store
-});
-
-// Toolbar
-var myToolbar = new Ext.Toolbar({
-    dock : 'top',
-    title: 'To-Do',
-    items: [
-        { text: 'New'}
-    ]
-});
-
-new Ext.Application({
-        launch: function() {
-        	list.show();
-        	// Main Panel
-        	var myPanel = new Ext.Panel({
-        		fullscreen: true,
-        		style: 'background-color:#E3E4FA;',
-        		items: [list],
-        	    dockedItems: [myToolbar],
-        	});
-        }
-    });
